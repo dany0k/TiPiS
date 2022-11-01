cb925d83783f0ae39ed2716af229e66972f744bd
Den-Laptop <zmaev.vsu@gmail.com>
2022-11-01 21:42:54 +0300

    Fixed graphs

--- lab2/main.py
+++ lab2/main.py
@@ -10,14 +10,14 @@ INFORMATIONAL_FREQUENCY: int = 20
 FI: float = 0.0
 
 SCREEN_SIZE: int = 10
-GRAPH_W: float = 0.2
-GRAPH_H: float = 0.2
-BOTTOM_MARGIN: float = 0.78
-START_MARGIN: float = 0.04
+GRAPH_W: float = 0.3
+GRAPH_H: float = 0.3
+BOTTOM_MARGIN: float = 0.68
+START_MARGIN: float = 0.03
 BAR_THICKNESS: float = 0.1
 
-START_CORDS: int = -200
-END_CORDS: int = 200
+START_CORDS: int = 0
+END_CORDS: int = 400
 POINTS_AMOUNT: int = 1000
 MODULATION_COEFFICIENT: float = 1.0
 
@@ -44,6 +44,8 @@ def informational_signal(amplitude: int, inf_freq: int, t: np.ndarray) -> np.nda
 ###############
 # Modulations #
 ###############
+
+
 def amplitude_modulate(amplitude: int, carrier_freq: int, inf_freq: int, t: np.ndarray) -> np.ndarray:
     return informational_signal(amplitude, inf_freq, t) * carrier_signal(amplitude, carrier_freq, t)
 
@@ -77,7 +79,42 @@ def phase_modulation(amplitude: int, m: float, carrier_freq: int, inf_freq: int,
 
 
 def any_modulation_spectre(fun: np.ndarray, points_amount: int) -> np.ndarray:
-    return np.abs(scipy.fft.fft(fun)) / (points_amount / 2)
+    t: np.ndarray = np.linspace(START_CORDS, END_CORDS, points_amount)
+    spectre: np.ndarray = np.abs(scipy.fft.fft(fun)) / (points_amount / 2)
+    error_point = 0.03
+    for i in range(points_amount):
+        if i > points_amount / 2 or spectre[i] < error_point:
+            spectre[i] = 0
+    return spectre
+
+
+def draw_graphs(gr, t: np.ndarray,
+                amp_mod_sig: np.ndarray, fr_mod_sig: np.ndarray, ph_mod_sig: np.ndarray,
+                amp_mod_spectre: np.ndarray, fr_mod_spectre: np.ndarray, ph_mod_spectre: np.ndarray) -> None:
+    fun_array = [
+        [amp_mod_sig, fr_mod_sig, ph_mod_sig],
+        [amp_mod_spectre, fr_mod_spectre, ph_mod_spectre]
+    ]
+
+    start_offset: float = 0.33
+    bottom_offset: float = 0.33
+    left_limit: int = START_CORDS - 5
+    right_limit: float = END_CORDS / 4
+    gr.add_axes([START_MARGIN, BOTTOM_MARGIN, GRAPH_W, GRAPH_H])
+    plt.plot(t, fun_array[0][0])
+    gr.add_axes([START_MARGIN + start_offset, BOTTOM_MARGIN, GRAPH_W, GRAPH_H])
+    plt.plot(t, fun_array[0][1])
+    gr.add_axes([START_MARGIN + start_offset * 2, BOTTOM_MARGIN, GRAPH_W, GRAPH_H])
+    plt.plot(t, fun_array[0][2])
+    gr.add_axes([START_MARGIN, BOTTOM_MARGIN - bottom_offset, GRAPH_W, GRAPH_H])
+    plt.xlim(left_limit, right_limit)
+    plt.bar(t, fun_array[1][0])
+    gr.add_axes([START_MARGIN + start_offset, BOTTOM_MARGIN - bottom_offset, GRAPH_W, GRAPH_H])
+    plt.xlim(left_limit, right_limit)
+    plt.bar(t, fun_array[1][1])
+    gr.add_axes([START_MARGIN + start_offset * 2, BOTTOM_MARGIN - bottom_offset, GRAPH_W, GRAPH_H])
+    plt.xlim(left_limit, right_limit)
+    plt.bar(t, fun_array[1][2])
 
 
 def main(amplitude: int = AMPLITUDE, carrier_frequency: int = CARRIER_FREQUENCY,
@@ -92,30 +129,9 @@ def main(amplitude: int = AMPLITUDE, carrier_frequency: int = CARRIER_FREQUENCY,
     fr_mod_spectre: np.ndarray = any_modulation_spectre(fr_mod_sig, points_amount)
     ph_mod_spectre: np.ndarray = any_modulation_spectre(ph_mod_sig, points_amount)
 
-    plt.title('Amplitude')
-    plt.plot(t, amp_mod_sig)
-    plt.show()
-
-    plt.title('Freq')
-    plt.plot(t, fr_mod_sig)
-    plt.show()
-
-    plt.title('Phase')
-    plt.plot(t, ph_mod_sig)
-    plt.show()
-
-    ######################
-
-    plt.title('Amplitude')
-    plt.plot(t, amp_mod_spectre)
-    plt.show()
-
-    plt.title('Freq')
-    plt.plot(t, fr_mod_spectre)
-    plt.show()
-
-    plt.title('Phase')
-    plt.plot(t, ph_mod_spectre)
+    gr = plt.figure()
+    plt.gcf().set_size_inches(SCREEN_SIZE, SCREEN_SIZE)
+    draw_graphs(gr, t, amp_mod_sig, fr_mod_sig, ph_mod_sig, amp_mod_spectre, fr_mod_spectre, ph_mod_spectre)
     plt.show()
 
 
